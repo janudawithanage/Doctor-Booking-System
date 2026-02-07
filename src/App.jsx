a2a311759eeeb6d9359fab5a2f89795f77c76e94
P. W. J. CHAMANDITH <withanagejanuda@gmail.com>
2026-02-07 23:20:31 +0530

    Add Navbar component and update routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'  
 import Home from './pages/home'
-import Doctors from './pages/Doctors'
-import Login from './pages/Login'
-import About from './pages/About'
+import Doctors from './pages/doctors'
+import Login from './pages/login'
+import About from './pages/about'
 import Contact from './pages/contact'
 import MyProfile from './pages/myprofile'
 import MyAppointments from './pages/myappointments'
 import Appointment from './pages/Appointment'
+import Navbar from './components/NAvbar'
     
 const App = () => {
   return (
     <div className='mx-4 sm:mx-[10%]'>
+      <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/doctors' element={<Doctors />} />
