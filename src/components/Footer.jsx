c01cba2f0c4b808d46264237587cceb849d19270
P. W. J. CHAMANDITH <withanagejanuda@gmail.com>
2026-02-09 16:23:30 +0530

    Add Banner component to Home page and update Footer contact details

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -31,7 +31,7 @@ const Footer = () => {
           <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
           <ul className='flex flex-col gap-2 text-gray-600'>
             <li>+1-212-456-7890</li>
-            <li>greatstackdev@gmail.com</li>
+            <li>janudawithanage@gmail.com</li>
           </ul>
         </div>
       </div>
@@ -39,7 +39,7 @@ const Footer = () => {
       {/* ----- Copyright Text ----- */}
       <div>
         <hr />
-        <p className='py-5 text-sm text-center'>Copyright © 2026 GreatStack - All Right Reserved.</p>
+        <p className='py-5 text-sm text-center'>Copyright © 2026 JanudaWithanage - All Right Reserved.</p>
       </div>
     </div>
   )
