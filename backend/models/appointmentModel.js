1378112ffadeb8693f96863d2eb505725ec59941
P. W. J. CHAMANDITH <withanagejanuda@gmail.com>
2026-03-11 23:45:10 +0530

    style: add appointment model for managing appointment data

---
+++ backend/models/appointmentModel.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose'
+
+const appointmentSchema = new mongoose.Schema({
+    userId: { type: String, required: true },
+    docId: { type: String, required: true },
+    slotDate: { type: String, required: true },
+    slotTime: { type: String, required: true },
+    userData: { type: Object, required: true },
+    docData: { type: Object, required: true },
+    amount: { type: Number, required: true },
+    payment: { type: Boolean, default: false },
+    cancelled: { type: Boolean, default: false },
+    isCompleted: { type: Boolean, default: false }
+}, { timestamps: true })
+
+const appointmentModel = mongoose.models.appointment || mongoose.model('appointment', appointmentSchema)
+
+export default appointmentModel
